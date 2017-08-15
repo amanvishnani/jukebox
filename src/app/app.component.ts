296d9379e6d44143a23183cf0b3392119c172a53
Aman Vishnani <aman62@gmail.com>
2017-08-16 01:07:17 +0530

    My Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { MdSidenav } from "@angular/material";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  sideNav: number;
+  profile: any = {};
+  constructor() {
+    this.title = 'app';
+    this.sideNav = 0;
+    this.profile.pic = "assets/img/user.png";
+  }
+  openNav(sidenav: MdSidenav) {
+    this.sideNav = 1;
+    sidenav.open();
+  }
+  closeNav(sidenav: MdSidenav) {
+    this.sideNav = 0;
+    sidenav.close();
+  }
+  swipe(e, sidenav: MdSidenav) {
+    if(e==='swiperight') {
+      sidenav.open();
+    }
+    if(e==='swipeleft') {
+      sidenav.close();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import 'hammerjs';
+import { AppRoutingModule } from "./app-routing/app-routing.module";
+
+
+import { MdSidenavModule, MdButtonModule, MdCheckboxModule, MdToolbarModule, MdIconModule } from "@angular/material";
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MdSidenavModule,
+    MdButtonModule,
+    MdToolbarModule,
+    MdIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
